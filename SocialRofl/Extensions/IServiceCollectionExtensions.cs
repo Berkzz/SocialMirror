3066bf78c7990bbf83795773bf4a5bf8c0f66be1
Максим Попов <crissgmod@gmail.com>
2022-03-14 14:10:48 +0400

    Clean Program.cs

---
+++ SocialRofl/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using SocialRofl.Data;
+using SocialRofl.Interfaces;
+using SocialRofl.Models.Database;
+using System.Text;
+
+namespace SocialRofl.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static void ConfigureSwagger(this IServiceCollection collection)
+        {
+            collection.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SocialRofl", Version = "v1.0.0" });
+
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", securitySchema);
+
+                var securityRequirement = new OpenApiSecurityRequirement
+                {
+                    { securitySchema, new[] { "Bearer" } }
+                };
+
+                c.AddSecurityRequirement(securityRequirement);
+
+            });
+        }
+        
+        public static void ConfigureIdentity(this IServiceCollection collection, IConfiguration config)
+        {
+            collection.AddIdentityCore<User>(options => options.Password = new PasswordOptions
+            {
+                RequireDigit = false,
+                RequiredLength = 1,
+                RequiredUniqueChars = 0,
+                RequireLowercase = false,
+                RequireNonAlphanumeric = false,
+                RequireUppercase = false
+            }).AddEntityFrameworkStores<DataContext>();
+            var mySecret = config.GetValue<string>("SecurityKey");
+            var mySecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(mySecret));
+            collection.AddAuthentication("Bearer").AddJwtBearer("Bearer", options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = mySecurityKey
+                };
+            });
+        }
+
+        public static void AddLogic(this IServiceCollection collection)
+        {
+            collection.AddScoped<Logic.AttachmentChecker>();
+            collection.AddScoped<Logic.Auth>();
+            collection.AddScoped<Logic.PhotoLogic>();
+            collection.AddScoped<Logic.FriendsLogic>();
+            collection.AddScoped<Logic.SearchLogic>();
+            collection.AddScoped<Logic.UserLogic>();
+            collection.AddScoped<Logic.WallLogic>();
+            collection.AddScoped<IHashGenerator, Logic.HashGenerator>();
+        }
+    }
+}
--- SocialRofl/Program.cs
+++ SocialRofl/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using SocialRofl.Data;
+using SocialRofl.Extensions;
 using SocialRofl.Interfaces;
 using SocialRofl.Models.Database;
 using System.Text;
@@ -10,69 +11,18 @@ using Logic = SocialRofl.Logic;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "SocialRofl", Version = "v1.0.0" });
 
-    var securitySchema = new OpenApiSecurityScheme
-    {
-        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer",
-        Reference = new OpenApiReference
-        {
-            Type = ReferenceType.SecurityScheme,
-            Id = "Bearer"
-        }
-    };
+builder.Services.ConfigureSwagger();
 
-    c.AddSecurityDefinition("Bearer", securitySchema);
-
-    var securityRequirement = new OpenApiSecurityRequirement
-                {
-                    { securitySchema, new[] { "Bearer" } }
-                };
-
-    c.AddSecurityRequirement(securityRequirement);
-
-});
-builder.Configuration.AddJsonFile("appsettings.json");
 builder.Services.AddDbContext<DataContext>();
-builder.Services.AddIdentityCore<User>(options => options.Password = new PasswordOptions
-{
-    RequireDigit = false,
-    RequiredLength = 1,
-    RequiredUniqueChars = 0,
-    RequireLowercase = false,
-    RequireNonAlphanumeric = false,
-    RequireUppercase = false
-})
-    .AddEntityFrameworkStores<DataContext>();
-var mySecret = builder.Configuration.GetValue<string>("SecurityKey");
-var mySecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(mySecret));
-builder.Services.AddAuthentication("Bearer").AddJwtBearer("Bearer", options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = mySecurityKey
-    };
-});
 
-builder.Services.AddScoped<Logic.AttachmentChecker>();
-builder.Services.AddScoped<Logic.Auth>();
-builder.Services.AddScoped<Logic.PhotoLogic>();
-builder.Services.AddScoped<Logic.FriendsLogic>();
-builder.Services.AddScoped<Logic.SearchLogic>();
-builder.Services.AddScoped<Logic.UserLogic>();
-builder.Services.AddScoped<Logic.WallLogic>();
-builder.Services.AddScoped<IHashGenerator, Logic.HashGenerator>();
+builder.Configuration.AddJsonFile("appsettings.json");
+
+builder.Services.ConfigureIdentity(builder.Configuration);
+
+builder.Services.AddLogic();
 
 var app = builder.Build();
 
